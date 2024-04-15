c6b6e4e07a57fb28b74ac61b278b274a963b4538
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-04-15 12:04:40 -0300

    Add Resources controles for /question and /categories endpoint

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-const {
-  fetchTriviaCategories,
-  fetchTriviaQuestion,
-  getFrontendQuestion,
-} = require('./utils/triviaAPI');
 const triviaController = require('./controllers/trivia');
-const hasToken = require('./middlewares/hasToken');
+const resourcesController = require('./controllers/resources');
 
 const app = express();
 
@@ -29,40 +24,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(resourcesController);
 app.use(triviaController);
-
-app.get('/categories', async (req, res) => {
-  try {
-    const categories = await fetchTriviaCategories();
-    res.send({
-      status: 200,
-      data: categories,
-      message: 'Categories fetched with success.',
-    });
-  } catch (error) {
-    // TODO: Implement a catch all Error method.
-    res.send({
-      status: 500,
-      message: error.message || 'Categories fetching failed. Try again later.',
-    });
-  }
-});
-
-app.get('/question', hasToken, async (req, res) => {
-  const question = await fetchTriviaQuestion(req.session.user);
-  const user = req.session.user;
-  req.session.regenerate(function (err) {
-    if (err) next(err);
-    req.session.user = user;
-    req.session.question = question;
-  });
-
-  const formattedQuestion = getFrontendQuestion(question);
-
-  req.session.save(function (err) {
-    if (err) return next(err);
-    res.send(formattedQuestion);
-  });
-});
 
 app.listen(process.env.SERVER_PORT);
---
+++ controllers/resources.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const {
+  fetchTriviaCategories,
+  fetchTriviaQuestion,
+  getFrontendQuestion,
+} = require('../utils/triviaAPI');
+const hasToken = require('../middlewares/hasToken');
+
+const router = express.Router();
+
+router.get('/categories', async (req, res) => {
+  try {
+    const categories = await fetchTriviaCategories();
+    res.send({
+      status: 200,
+      data: categories,
+      message: 'Categories fetched with success.',
+    });
+  } catch (error) {
+    // TODO: Implement a catch all Error method.
+    res.send({
+      status: 500,
+      message: error.message || 'Categories fetching failed. Try again later.',
+    });
+  }
+});
+
+router.get('/question', hasToken, async (req, res) => {
+  const question = await fetchTriviaQuestion(req.session.user);
+  const user = req.session.user;
+  req.session.regenerate(function (err) {
+    if (err) next(err);
+    req.session.user = user;
+    req.session.question = question;
+  });
+
+  const formattedQuestion = getFrontendQuestion(question);
+
+  req.session.save(function (err) {
+    if (err) return next(err);
+    res.send(formattedQuestion);
+  });
+});
+
+module.exports = router;
