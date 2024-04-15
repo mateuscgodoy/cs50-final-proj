c437b27bd7ce1b93e46bad65832e6bfa3470858a
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-04-15 10:31:45 -0300

    Add function to fetch a single question from Trivia API

--- utils/triviaAPI.js
+++ utils/triviaAPI.js
@@ -61,13 +61,84 @@ async function fetchTriviaToken() {
 }
 
 /**
+ * Fetches the Trivia API for a question based on the user current information.
+ * @param {Object} user The user object as stored on the current session.
+ * @returns {Object} A single question object as received directly from the API.
+ */
+async function fetchTriviaQuestion(user) {
+  //* URL EXAMPLE: https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple&token=SOMEUSERTOKEN
+  const queryUrl = getQuestionQueryURL(user);
+  try {
+    let code;
+    do {
+      const response = await fetch(
+        `https://opentdb.com/api.php?${queryUrl.toString()}`
+      );
+      const { response_code, results } = await response.json();
+      code = response_code;
+
+      if (code === 5) {
+        await delayFunction(3000);
+      } else if (code === 0) {
+        return results[0];
+      } else {
+        throw new Error(
+          'Trivia API could not return a question. Please, try again later.'
+        );
+      }
+    } while (code === 5);
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Creates a custom URL parameters query string to be used to retrieve a question.
+ * @param {Object} user The user object as stored on the session.
+ * @returns {URLSearchParams} The query search parameters.
+ */
+function getQuestionQueryURL(user) {
+  const { token, selectedCategories, questionsAnswered } = user;
+  const category = getRandomCategory(selectedCategories);
+  const difficulty = getDifficulty(questionsAnswered);
+  const params = { amount: 1, type: 'multiple', token, category, difficulty };
+  return new URLSearchParams(params);
+}
+
+/**
+ * Selects a random category among the options chose by the user.
+ * @param {number[]} selectedCategories The categories selected by the user.
+ * @returns {number} A random category.
+ */
+function getRandomCategory(selectedCategories) {
+  const randomIndex = Math.floor(Math.random() * selectedCategories.length);
+  return selectedCategories[randomIndex];
+}
+
+/**
+ * Determines the difficulty for a question based on how many the user already answered.
+ * @param {number} questionsAnswered The current amount of questions answered.
+ * @returns {string} The difficulty that the question should be.
+ */
+function getDifficulty(questionsAnswered) {
+  let difficulty = 'easy';
+  if (questionsAnswered > 10) {
+    difficulty = 'hard';
+  } else if (questionsAnswered > 6) {
+    difficulty = 'medium';
+  }
+  return difficulty;
+}
+
+/**
  * Creates a new user object to control different aspects of the game.
  * @param {string} token The token received from Trivia API.
  * @returns {Object} An object containing all the elements that represent an user.
  */
-function createUser(token) {
+function createUser(token, selectedCategories) {
   return {
     token,
+    selectedCategories,
     questionsAnswered: 0,
     lifelines: {
       '50:50': true,
@@ -78,4 +149,18 @@ function createUser(token) {
   };
 }
 
-module.exports = { fetchTriviaCategories, fetchTriviaToken, createUser };
+/**
+ * A utility function to pause the execution of a function, to avoid API time constraint.
+ * @param {number} delay The delay amount in ms.
+ * @returns A promise that will resolve once the delay elapsed.
+ */
+function delayFunction(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+module.exports = {
+  fetchTriviaCategories,
+  fetchTriviaToken,
+  fetchTriviaQuestion,
+  createUser,
+};
